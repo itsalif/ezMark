9dc3d0c87f6f1620e9857805b8126b2991fe63fc
Albertas Agejevas <alga@pov.lt>
2012-01-23 18:16:57 +0200

    Removed trailing whitespace.

--- js/jquery.ezmark.js
+++ js/jquery.ezmark.js
@@ -1,31 +1,31 @@
 /**
- * ezMark - A Simple Checkbox and Radio button Styling plugin. 
+ * ezMark - A Simple Checkbox and Radio button Styling plugin.
  * This plugin allows you to use a custom Image for Checkbox or Radio button. Its very simple, small and easy to use.
- * 
+ *
  * Copyright (c) Abdullah Rubiyath <http://www.itsalif.info/>.
  * Released under MIT License
- * 
+ *
  * Files with this plugin:
  * - jquery.ezmark.js
  * - ezmark.css
- * 
+ *
  * <usage>
  * At first, include both the css and js file at the top
- * 
- * Then, simply use: 
+ *
+ * Then, simply use:
  * 	$('selector').ezMark([options]);
- *  
+ *
  * [options] accepts following JSON properties:
  *  checkboxCls - custom Checkbox Class
  *  checkedCls  - checkbox Checked State's Class
  *  radioCls    - custom radiobutton Class
  *  selectedCls - radiobutton's Selected State's Class
- *  
+ *
  * </usage>
- * 
+ *
  * View Documention/Demo here:
  * http://www.itsalif.info/content/ezmark-jquery-checkbox-radiobutton-plugin
- * 
+ *
  * @author Abdullah Rubiyath
  * @version 1.0
  * @date June 27, 2010
@@ -33,10 +33,10 @@
 
 (function($) {
   $.fn.ezMark = function(options) {
-	options = options || {}; 
-	var defaultOpt = { 
-		checkboxCls   	: options.checkboxCls || 'ez-checkbox' , radioCls : options.radioCls || 'ez-radio' ,	
-		checkedCls 		: options.checkedCls  || 'ez-checked'  , selectedCls : options.selectedCls || 'ez-selected' , 
+	options = options || {};
+	var defaultOpt = {
+		checkboxCls   	: options.checkboxCls || 'ez-checkbox' , radioCls : options.radioCls || 'ez-radio' ,
+		checkedCls 		: options.checkedCls  || 'ez-checked'  , selectedCls : options.selectedCls || 'ez-selected' ,
 		hideCls  	 	: 'ez-hide'
 	};
     return this.each(function() {
@@ -45,32 +45,32 @@
     	// for checkbox
     	if( $this.attr('type') == 'checkbox') {
     		$this.addClass(defaultOpt.hideCls).wrap(wrapTag).change(function() {
-    			if( $(this).is(':checked') ) { 
-    				$(this).parent().addClass(defaultOpt.checkedCls); 
-    			} 
+    			if( $(this).is(':checked') ) {
+    				$(this).parent().addClass(defaultOpt.checkedCls);
+    			}
     			else {	$(this).parent().removeClass(defaultOpt.checkedCls); 	}
     		});
-    		
+
     		if( $this.is(':checked') ) {
-				$this.parent().addClass(defaultOpt.checkedCls);    		
+				$this.parent().addClass(defaultOpt.checkedCls);
     		}
-    	} 
+    	}
     	else if( $this.attr('type') == 'radio') {
 
     		$this.addClass(defaultOpt.hideCls).wrap(wrapTag).change(function() {
     			// radio button may contain groups! - so check for group
    				$('input[name="'+$(this).attr('name')+'"]').each(function() {
-   	    			if( $(this).is(':checked') ) { 
-   	    				$(this).parent().addClass(defaultOpt.selectedCls); 
+   	    			if( $(this).is(':checked') ) {
+   	    				$(this).parent().addClass(defaultOpt.selectedCls);
    	    			} else {
-   	    				$(this).parent().removeClass(defaultOpt.selectedCls);     	    			
+   	    				$(this).parent().removeClass(defaultOpt.selectedCls);
    	    			}
    				});
     		});
-    		
+
     		if( $this.is(':checked') ) {
-				$this.parent().addClass(defaultOpt.selectedCls);    		
-    		}    		
+				$this.parent().addClass(defaultOpt.selectedCls);
+    		}
     	}
     });
   }
